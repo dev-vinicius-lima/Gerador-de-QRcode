bf49556363d7fdab53ce2aba79a295437745fd25
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-01-13 12:53:16 -0400

    ajustei qr code

--- js/script.js
+++ js/script.js
@@ -2,19 +2,20 @@ const container = document.querySelector('.container');
 const qrCodeBtn = document.querySelector('.qrForm button');
 
 const qrCodeInput = document.querySelector('.qrForm input')
-const qrCodeImg = document.querySelector('.qrCode')
+const qrCodeImg = document.querySelector('#imgQr')
 
 function generateQrCode() {
     const qrCodeInputValue = qrCodeInput.value
 
     if (!qrCodeInputValue) return;// validação
     qrCodeBtn.innerText = "Gerando Código..."
+    qrCodeImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
 
-    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
-    container.classList.add('active')
-    qrCodeBtn.innerText='Código Criado!'
+    qrCodeImg.addEventListener('load', ()=> {
+      container.classList.add('active')
+      qrCodeBtn.innerText='Código Criado!'
+    })
 
-    
 }
 qrCodeBtn.addEventListener('click', () => {
   generateQrCode();
