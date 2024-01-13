7d843b3b7bcf5680684823eb876d6022344fc5f7
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-01-13 16:27:45 -0400

    ajustei

--- js/script.js
+++ js/script.js
@@ -1,22 +1,35 @@
 const container = document.querySelector('.container');
 const qrCodeBtn = document.querySelector('.qrForm button');
 
-const qrCodeInput = document.querySelector('.qrForm input')
-const qrCodeImg = document.querySelector('#imgQr')
+const qrCodeInput = document.querySelector('.qrForm input');
+const qrCodeImg = document.querySelector('#imgQr');
 
 function generateQrCode() {
-    const qrCodeInputValue = qrCodeInput.value
+  const qrCodeInputValue = qrCodeInput.value;
 
-    if (!qrCodeInputValue) return;// validação
-    qrCodeBtn.innerText = "Gerando Código..."
-    qrCodeImg.src =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+  if (!qrCodeInputValue) return; // validação
+  qrCodeBtn.innerText = 'Gerando Código...';
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
 
-    qrCodeImg.addEventListener('load', ()=> {
-      container.classList.add('active')
-      qrCodeBtn.innerText='Código Criado!'
-    })
-
+  qrCodeImg.addEventListener('load', () => {
+    container.classList.add('active');
+    qrCodeBtn.innerText = 'Código Criado!';
+  });
 }
 qrCodeBtn.addEventListener('click', () => {
   generateQrCode();
+});
+
+// adicionar evento de key com enter
+qrCodeInput.addEventListener('keydown', (event) => {
+  if (event.code == 'Enter') {
+    generateQrCode();
+  }
+});
+
+qrCodeInput.addEventListener('keyup', () => {
+  if (!qrCodeInput.value) {
+    container.classList.remove('active');
+    qrCodeBtn.innerText = 'Gerar QR Code';
+  }
 });
