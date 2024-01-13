2f1edf0f8163536e0241ad2d56f60e4a27d84349
Vinicius Lima <viniciuslimaes@hotmail.com>
2024-01-13 11:53:36 -0400

    criei um gerador de QR Code

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const qrCodeBtn = document.querySelector('.qrForm button');
+
+const qrCodeInput = document.querySelector('.qrForm input')
+const qrCodeImg = document.querySelector('.qrCode')
+
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value
+
+    if (!qrCodeInputValue) return;// validação
+    qrCodeBtn.innerText = "Gerando Código..."
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+    container.classList.add('active')
+    qrCodeBtn.innerText='Código Criado!'
+
+    
+}
+qrCodeBtn.addEventListener('click', () => {
+  generateQrCode();
+});
